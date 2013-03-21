ca8f9f28acc60f64cb0f2d0a5f9d50e2b8370cfd
CarlKenner <carl.kenner@gmail.com>
2013-03-22 02:28:09 +1030

    Added vrpn as a submodule, and integrated it into Doom 3. You MUST use "Recursive" now when cloning or updating this repository. I haven't done VRPN headtracking yet, but that's the easy part. The hard part is finished.

--- neo/sys/win32/in_motion_sensor.cpp
+++ neo/sys/win32/in_motion_sensor.cpp
@@ -3,6 +3,8 @@
 #include "../../idlib/precompiled.h"
 
 #include "../../../dependencies/libfreespace/include/freespace/freespace.h"
+#include "../../../dependencies/vrpn/vrpn_Tracker.h"
+#include "../../../dependencies/vrpn/quat/quat.h"
 #include "in_motion_sensor.h"
 #include "MahonyAHRS.h"
 
@@ -20,6 +22,13 @@ void IN_MotionSensor_Thread(void);
 FreespaceDeviceId device;
 float angles[3];
 
+vrpn_Tracker_Remote* vrpnTracker = new vrpn_Tracker_Remote("Tracker0@localhost");
+
+void VRPN_CALLBACK handle_tracker(void* userData, const vrpn_TRACKERCB t )
+{
+	double euler[3];
+	q_to_euler((double *)t.quat, euler);
+}
 
 void IN_MotionSensor_Init(void)
 {
@@ -33,26 +42,26 @@ void IN_MotionSensor_Init(void)
 	// Initialize the freespace library
 	rc = freespace_init();
 	if (rc != FREESPACE_SUCCESS) {
-		printf("Initialization error. rc=%d\n", rc);
+		//printf("Initialization error. rc=%d\n", rc);
 		return;
 	}
 
 	/** --- START EXAMPLE INITIALIZATION OF DEVICE -- **/
 	rc = freespace_getDeviceList(&device, 1, &numIds);
 	if (numIds == 0) {
-		printf("MotionSensor: Didn't find any devices.\n");
+		//printf("MotionSensor: Didn't find any devices.\n");
 		return;
 	}
 
 	rc = freespace_openDevice(device);
 	if (rc != FREESPACE_SUCCESS) {
-		printf("MotionSensor: Error opening device: %d\n", rc);
+		//printf("MotionSensor: Error opening device: %d\n", rc);
 		return;
 	}
 
 	rc = freespace_flush(device);
 	if (rc != FREESPACE_SUCCESS) {
-		printf("MotionSensor: Error flushing device: %d\n", rc);
+		//printf("MotionSensor: Error flushing device: %d\n", rc);
 		return;
 	}
 
@@ -63,7 +72,7 @@ void IN_MotionSensor_Init(void)
 
 	rc = freespace_sendMessage(device, &message);
 	if (rc != FREESPACE_SUCCESS) {
-		printf("freespaceInputThread: Could not send message: %d.\n", rc);
+		//printf("freespaceInputThread: Could not send message: %d.\n", rc);
 	}
 
 	CreateThread(NULL, //Choose default security
